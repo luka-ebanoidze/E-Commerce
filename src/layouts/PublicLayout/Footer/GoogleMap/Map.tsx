743eb918a46350b2676e5d41f2d386d094a7130f
luka-ebanoidze <ebanoidzelukaa@gmail.com>
2023-06-16 02:12:05 +0400

    feature(map)

---
+++ src/layouts/PublicLayout/Footer/GoogleMap/Map.tsx
@@ -0,0 +1,10 @@
+
+export function GoogleMap() {
+  return (
+    <iframe
+      className="w-full h-full"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.3319992907814!2d-122.0115469235959!3d37.33464803739024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb596e9e188fd%3A0x3b0d8391510688f0!2sApple%20Park!5e0!3m2!1sen!2sge!4v1686866658332!5m2!1sen!2sge"
+      loading="lazy"
+    ></iframe>
+  );
+}
