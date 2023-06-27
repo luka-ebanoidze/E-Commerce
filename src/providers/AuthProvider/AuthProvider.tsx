4ea4e9529c07af18071707758d25734591097352
luka-ebanoidze <ebanoidzelukaa@gmail.com>
2023-06-27 23:30:44 +0400

    authFix

--- src/providers/AuthProvider/AuthProvider.tsx
+++ src/providers/AuthProvider/AuthProvider.tsx
@@ -1,4 +1,5 @@
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
 
 import { AuthContext } from "@src/context/AuthContext";
 import { TAuthorizationStage } from "@src/types/auth.types";
@@ -7,6 +8,15 @@ export function AuthProvider({ children }: PropsWithChildren) {
   const [status, setStatus] = useState<TAuthorizationStage>(
     TAuthorizationStage.UNAUTHORIZED
   );
+
+   useEffect(() => {
+     const token = localStorage.getItem("acces-token");
+     if (token) {
+       const decodedToken = jwt_decode(token);
+
+       setStatus(TAuthorizationStage.AUTHORIZED)
+     }
+   }, []);
 
   return (
     <AuthContext.Provider value={{ status, setStatus }}>
