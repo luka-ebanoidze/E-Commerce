8062f03e6d9272f9371c9831177003bbb002be91
luka-ebanoidze <ebanoidzelukaa@gmail.com>
2023-07-11 08:04:57 +0400

    langFix

--- src/views/public/routes/ProductView/SimilarProducts/SimilarProducts.tsx
+++ src/views/public/routes/ProductView/SimilarProducts/SimilarProducts.tsx
@@ -1,6 +1,42 @@
+import { ProductsContainer } from "@src/components/ProductsContainer";
+import { instance } from "@src/utils/axiosInstance";
+import { useEffect, useState } from "react";
 
-export function SimilarProducts() {
+export function SimilarProducts(props: any) {
+  const { category } = props;
+  const [products, setProducts] = useState([]);
+
+  async function getSimilarProducts() {
+    try {
+      if (category) {
+        const resp = await instance.get(
+          `/products?take=4&category=${category}`
+        );
+        setProducts(resp.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getSimilarProducts();
+  }, [category]);
+
   return (
-    <div className="w-full h-[300px] bg-[green] my-14">Similar Products</div>
+    <div className="w-full flex flex-wrap  justify-center gap-5 my-14">
+      {products?.map((product: any) => (
+        <ProductsContainer
+          key={product.id}
+          title={product.title}
+          price={product.price}
+          thumbnail={product.thumbnail}
+          rating={product.rating}
+          id={product.id}
+          item={product}
+          category={product.category}
+        />
+      ))}
+    </div>
   );
 }
